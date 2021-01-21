d716c124c5c4a19f0c42f0f2e93c7b4ce88a10a6
Zhigang Dong <51614718+jzdzg@users.noreply.github.com>
2021-01-21 15:47:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "current time: ".date('Y/m/d h:i:s',time()) ."host name: " .gethostname();
+echo "current time: ".date('Y/m/d h:i:s',time()) ." host name: " .getenv('HTTP_HOST');
 
 ?>
