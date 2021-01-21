6ed2cf7b8ecca02e05471b3eb732c29d4aca140d
Zhigang Dong <51614718+jzdzg@users.noreply.github.com>
2021-01-21 14:40:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
-#$msg= date('Y/m/d h:i:s',time());
-#echo "current time:".$msg;
-echo "current time:".date('Y/m/d h:i:s',time());
+echo "current time: ".date('Y/m/d h:i:s',time());
 echo "<br>add one more line for demo."
 
 ?>
